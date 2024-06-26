da24c596956f61c822007deee6f5d6f0fc4913de
Alejandro Gómez <alejandrogomez2339@gmail.com>
2024-06-26 09:54:46 -0500

    User seeds

--- src/seed/data/seed-data.ts
+++ src/seed/data/seed-data.ts
@@ -13,17 +13,38 @@ interface SeedData {
 
 export const initialData: SeedData = {
     users: [
+        // CEO
         {
-            email: 'test1@google.com',
-            fullName: 'Test User 1',
-            password: bcrypt.hashSync('1passwordD', 10),
-            roles: ['admin']
+            email: 'jorge.estevez@estevezjor.mx',
+            password: bcrypt.hashSync('Estevez.ceo*', 10),
+            fullName: 'Jorge Estévez Abreu',
+            roles: ['ceo']
+        },
+        // General Director
+        {
+            email: 'fernanda.estevez@estevezjor.mx',
+            fullName: 'Ma. Fernanda Estévez Gonzalez',
+            password: bcrypt.hashSync('Estevez.director*', 10),
+            roles: ['generalDirector']
+        },
+        // Directiors
+        {
+            email: 'ingrid.estevez@estevezjor.mx',
+            fullName: 'Ingrid Martinez Baeza',
+            password: bcrypt.hashSync('Estevez.director*', 10),
+            roles: ['director']
         },
         {
-            email: 'test2@google.com',
-            fullName: 'Test User 2',
-            password: bcrypt.hashSync('1passwordD', 10),
-            roles: ['user', 'super']
+            email: 'brandon.estevez@estevezjor.mx',
+            fullName: 'Brandon Jhoan de Jesus Hernandez',
+            password: bcrypt.hashSync('Estevez.director*', 10),
+            roles: ['director']
+        },
+        {
+            email: 'elizabeth.estevez@estevezjor.mx',
+            fullName: 'Elizabeth Gonzalez Herrera',
+            password: bcrypt.hashSync('Estevez.director*', 10),
+            roles: ['director']
         }
     ]
 };
